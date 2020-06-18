a414f52773a1e89cbd7724bf951d966689b844c1
vorSherer <tcpma2011@gmail.com>
2020-06-17 19:27:06 -0700

    Initial commit.

---
+++ game_of_greed/game_of_greed.py
@@ -0,0 +1,16 @@
+class GameLogic:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def roll_dice(self, dice_count):
+        pass
+
+
+    @staticmethod
+    def calculate_score(self, dice_set):
+        pass
+
+
+class Banker:
+    pass
--- tests/test_game_of_greed.py
+++ tests/test_game_of_greed.py
@@ -1,5 +1,27 @@
 from game_of_greed import __version__
+from game_of_greed.game_of_greed import (
+    GameLogic,
+    Banker,
+)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_gamelogic_exists():
+    assert GameLogic
+
+
+def test_banker_exists():
+    assert Banker
+
+
+def test_roll_dice_exists():
+    assert GameLogic.roll_dice
+
+
+def test_calc_score_exists():
+    assert GameLogic.calculate_score
+
+
